382b927617c0ca5d9024c6c6c80a84e846c572e6
yerramreddyuday <uday.prasad@dojima.network>
2022-04-18 22:31:02 +0530

    dev: mod: Updated utils for retrieving balance of erc20 tokens

--- src/balance/erc20/utils.ts
+++ src/balance/erc20/utils.ts
@@ -5,82 +5,82 @@ export const contractData: {
     decimal: number;
   };
 } = {
-  BINANCE_USD: {
+  'binance usd': {
     contractAddress: "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
     tokenSymbol: "BUSD",
     decimal: 18,
   },
-  BNB: {
+  'bnb': {
     contractAddress: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52",
     tokenSymbol: "BNB",
     decimal: 18,
   },
-  CHAINLINK_TOKEN: {
+  'chainlink': {
     contractAddress: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
     tokenSymbol: "LINK",
     decimal: 18,
   },
-  CRONOS_COIN: {
+  'cronos': {
     contractAddress: "0xA0b73E1Ff0B80914AB6fe0444E65848C4C34450b",
     tokenSymbol: "CRO",
     decimal: 8,
   },
-  DAI_STABLECOIN: {
+  'dai': {
     contractAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     tokenSymbol: "DAI",
     decimal: 18,
   },
-  FANTOM: {
+  'fantom': {
     contractAddress: "0x4E15361FD6b4BB609Fa63C81A2be19d873717870",
     tokenSymbol: "FTM",
     decimal: 18,
   },
-  MAKER_DAO: {
+  'maker dao': {
     contractAddress: "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",
     tokenSymbol: "MKR",
     decimal: 18,
   },
-  MATIC: {
+  'matic': {
     contractAddress: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
     tokenSymbol: "MATIC",
     decimal: 18,
   },
-  SHIBA_INU: {
+  'shiba inu': {
     contractAddress: "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
     tokenSymbol: "SHIB",
     decimal: 18,
   },
-  TETHER: {
+  'tether': {
     contractAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
     tokenSymbol: "USDT",
     decimal: 6,
   },
-  UNISWAP: {
+  'uniswap': {
     contractAddress: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
     tokenSymbol: "UNI",
     decimal: 18,
   },
-  USDC: {
+  'usd coin': {
     contractAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     tokenSymbol: "USDC",
     decimal: 6,
   },
-  VE_CHAIN: {
+  've chain': {
     contractAddress: "0xD850942eF8811f2A866692A623011bDE52a462C1",
     tokenSymbol: "VEN",
     decimal: 18,
   },
-  WRAPPED_BTC: {
+  'wrapped bitcoin': {
     contractAddress: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
     tokenSymbol: "WBTC",
     decimal: 8,
   },
-  WRAPPED_LUNA: {
+  'terra': {
     contractAddress: "0xd2877702675e6cEb975b4A1dFf9fb7BAF4C91ea9",
     tokenSymbol: "LUNA",
     decimal: 18,
   },
-  WRAPPED_UST: {
+  'wrapped ust': {
     contractAddress: "0xa47c8bf37f92aBed4A126BDA807A7b7498661acD",
     tokenSymbol: "UST",
     decimal: 18,
@@ -88,22 +88,22 @@ export const contractData: {
 };
 
 export type tokenList =
-  | "BINANCE_USD"
-  | "BNB"
-  | "CHAINLINK_TOKEN"
-  | "CRONOS_COIN"
-  | "DAI_STABLECOIN"
-  | "FANTOM"
-  | "MAKER_DAO"
-  | "MATIC"
-  | "SHIBA_INU"
-  | "TETHER"
-  | "UNISWAP"
-  | "USDC"
-  | "VE_CHAIN"
-  | "WRAPPED_BTC"
-  | "WRAPPED_LUNA"
-  | "WRAPPED_UST";
+  | "binance usd"
+  | "bnb"
+  | "chainlink"
+  | "cronos"
+  | "dai"
+  | "fantom"
+  | "maker dao"
+  | "matic"
+  | "shiba inu"
+  | "tether"
+  | "uniswap"
+  | "usd coin"
+  | "ve chain"
+  | "wrapped bitcoin"
+  | "terra"
+  | "wrapped ust";
 
 export type tokenSymbolList =
   | "BUSD"
