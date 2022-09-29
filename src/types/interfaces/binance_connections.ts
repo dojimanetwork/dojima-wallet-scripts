2c1be7ce51a0c056fcd7460b12bdc9a92b61a010
ritwik-dojima <ritwik.reddy@dojima.network>
2022-09-29 15:21:50 +0530

    dev:mod:added url for mainnet

--- src/types/interfaces/binance_connections.ts
+++ src/types/interfaces/binance_connections.ts
@@ -4,15 +4,18 @@ import { NetworkType } from './network';
 
 export class BinanceConnection {
     _client : BncClient;
+    _clientUrl:string;
     _network: NetworkType;
 
     constructor(network:NetworkType){
         this._network =network;
         if(network === "mainnet"){
-          this._client =new BncClient("https://testnet-dex.binance.org/");
+          this._clientUrl = "https://dex.binance.org";
+          this._client =new BncClient(this._clientUrl);
         }
         else{
-          this._client =new BncClient("https://testnet-dex.binance.org/");
+          this._clientUrl = "https://testnet-dex.binance.org";
+          this._client =new BncClient(this._clientUrl);
         }
     }
 }
