048827d284fdc699abf713120621fdfeabb8476f
yerramreddyuday <uday.prasad@dojima.network>
2022-11-25 11:57:08 +0530

    dev: add: ethereum chain package

---
+++ src/core/ethereum/index.ts
@@ -0,0 +1,31 @@
+export * from './types'
+export * from './client'
+export {
+    call,
+    estimateApprove,
+    estimateCall,
+    estimateDefaultFeesWithGasPricesAndLimits,
+    getApprovalAmount,
+    getAssetAddress,
+    getDecimal,
+    getDefaultFees,
+    getDefaultGasPrices,
+    getTokenAddress,
+    getPrefix,
+    getFee,
+    isApproved,
+    isEthAsset,
+    validateAddress,
+    ETHAddress,
+    ETH_DECIMAL,
+    ETHPLORER_FREEKEY,
+    MAX_APPROVAL,
+} from './utils'
+
+import erc20ABI from './data/erc20.json'
+import routerABI from './data/routerABI.json'
+
+export const abi = {
+    router: routerABI,
+    erc20: erc20ABI,
+}
