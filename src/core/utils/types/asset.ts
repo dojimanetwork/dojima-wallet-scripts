d149e8e45d5e8d18e8fdbe101021c859fd8dd482
yerramreddyuday <uday.prasad@dojima.network>
2022-12-06 21:05:05 +0530

    dev: add: New types in core/utils

--- src/core/utils/types/asset.ts
+++ src/core/utils/types/asset.ts
@@ -6,3 +6,14 @@ export type Asset = {
     ticker: string
     synth: boolean
 }
+
+export type SwapAssetList = 'AR.AR' | 'BTC.BTC' | 'BNB.BNB' | 'D11K.DOJ' | 'DOT.DOT' | 'ETH.ETH' | 'SOL.SOL'
+
+export type InboundAddressResult = {
+    chain: string,
+    pub_key: string,
+    address: string,
+    halted: boolean,
+    gas_rate: string
+    router?: string,
+}
