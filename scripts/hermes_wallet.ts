5d9f3141a313d5b765c16667b2e1e9b52bff011c
yerramreddyuday <uday.prasad@dojima.network>
2022-12-09 09:03:26 +0530

    dev: mod: Hermes and Polka script update

--- scripts/hermes_wallet.ts
+++ scripts/hermes_wallet.ts
@@ -7,8 +7,8 @@ import {assetAmount, assetToBase} from "@d11k-ts/utils";
 
 async function checkHermes() {
     const phrase =
-        // "letter ethics correct bus asset pipe tourist vapor envelope kangaroo warm dawn";
-        "taxi act animal room trip column action real abstract kit acoustic rigid army jump drink merit over cupboard flat wasp clown ugly wealth coconut"
+        // "letter ethics correct bus asset pipe tourist vapor envelope kangaroo warm dawn"; // dojima1agwwyxg98x0smujpxnl63t45uua3yj4qrv5j58
+        "taxi act animal room trip column action real abstract kit acoustic rigid army jump drink merit over cupboard flat wasp clown ugly wealth coconut" // dojima15ca4lmfe9u6cc5x0cmqmw2wkvh6l4xdpr908km
 
     const hermesClient = new HermesSdkClient({phrase, network: Network.Testnet});
     const address = hermesClient.getAddress();
@@ -21,7 +21,7 @@ async function checkHermes() {
     console.log("Balance :: ", balance.toNumber());
     const data = await hermesClient.getTransactionData("6067CAC67EC27477B48C67237198964FFCD8FBC2F5C0CECA075D278192B45A1B", 'dojima1ek92jm68wkwv9zvjwn5gdgnt3vu4na3h5k5uje');
     console.log("Tx data : ", data);
-    // const data = await hermesClient.getDepositTransaction("A3DF03145C299F20E82E58762D26E2EBE0E9C663CBBF9DE57AE77FDA35634A0E");
+    // const data = await hermesClient.getDepositTransaction("FF17FEB3A0559132FCD3046F32B25ED71E7DB382786E444A27D890102AF471D8");
     // console.log("Deposit Tx data : ", data);
     const fees = await hermesClient.getFees();
     console.log("Fees : ", fees.fastest.amount().toNumber());
@@ -30,8 +30,12 @@ async function checkHermes() {
     let amount = assetToBase(assetAmount(1, DOJDECIMAL ))
     const hash = await hermesClient.transfer({amount, recipient: 'dojima1ek92jm68wkwv9zvjwn5gdgnt3vu4na3h5k5uje'});
     console.log("Tx hash : ", hash);
-    const depositHash = await hermesClient.deposit({amount, memo: `SWAP:AR.AR:7zzxJgYHgDlaURc3xt3wvLITPp6I8oIpYj_yg_xirb4`})
+    const depositHash = await hermesClient.deposit({amount, memo: `ADD:AR.AR:7zzxJgYHgDlaURc3xt3wvLITPp6I8oIpYj_yg_xirb4`})
     console.log('Deposit tx hash :: ', depositHash)
+    // const depositHash = await hermesClient.deposit({amount, memo: `ADD:DOT.DOT:5FKpdt1vdgQ2CNeNQXN9evodm4wBH6655R1GyV44tBdNgXP6`})
+    // console.log('Deposit tx hash :: ', depositHash)
+    // const depositHash = await hermesClient.deposit({amount, memo: `SWAP:AR.AR:7zzxJgYHgDlaURc3xt3wvLITPp6I8oIpYj_yg_xirb4`})
+    // console.log('Deposit tx hash :: ', depositHash)
 }
 
 (async () => {
--- scripts/polka_wallet.ts
+++ scripts/polka_wallet.ts
@@ -13,21 +13,21 @@ async function checkPolka() {
     const address = await polkaClient.getAddress();
     console.log("Address :: ", address);
     const bal = await polkaClient.getBalance(
-        '5FKpdt1vdgQ2CNeNQXN9evodm4wBH6655R1GyV44tBdNgXP6'
+        '5Gq3owRKkXLneUckXUc5UxKugXiqq78b71UQC4uHxcXFPdwH'
     );
     console.log("Balance :: ", bal);
-    // const fees = await polkaClient.getFees({recipient: '5Gq3owRKkXLneUckXUc5UxKugXiqq78b71UQC4uHxcXFPdwH', amount: 0.01});
-    // console.log("Fees : ", fees);
-    // const hash = await polkaClient.transfer({recipient: '5Gq3owRKkXLneUckXUc5UxKugXiqq78b71UQC4uHxcXFPdwH', amount: 0.01});
-    // console.log("Tx hash : ", hash);
-    // const inboundAddress = await polkaClient.getPolkadotInboundAddress();
-    // console.log('Inbound Address :: ', inboundAddress)
-    // const LPDefaultGasFee = await polkaClient.getDefaultLiquidityPoolGasFee();
-    // console.log('Liquidity pool default gas fee :: ', LPDefaultGasFee)
-    // const liquidityPoolHash = await polkaClient.addLiquidityPool(1, inboundAddress, 'dojima15ca4lmfe9u6cc5x0cmqmw2wkvh6l4xdpr908km')
-    // console.log('Liquidity pool hash : ', liquidityPoolHash)
-    // const swapHash = await polkaClient.swap(1,'D11K.DOJ', inboundAddress, 'dojima15ca4lmfe9u6cc5x0cmqmw2wkvh6l4xdpr908km')
-    // console.log('Swap tx hash : ', swapHash)
+    const fees = await polkaClient.getFees({recipient: '5FKpdt1vdgQ2CNeNQXN9evodm4wBH6655R1GyV44tBdNgXP6', amount: 0.01});
+    console.log("Fees : ", fees);
+    const hash = await polkaClient.transfer({recipient: '5FKpdt1vdgQ2CNeNQXN9evodm4wBH6655R1GyV44tBdNgXP6', amount: 0.01});
+    console.log("Tx hash : ", hash);
+    const inboundAddress = await polkaClient.getPolkadotInboundAddress();
+    console.log('Inbound Address :: ', inboundAddress)
+    const LPDefaultGasFee = await polkaClient.getDefaultLiquidityPoolGasFee();
+    console.log('Liquidity pool default gas fee :: ', LPDefaultGasFee)
+    const liquidityPoolHash = await polkaClient.addLiquidityPool(1, inboundAddress, 'dojima15ca4lmfe9u6cc5x0cmqmw2wkvh6l4xdpr908km')
+    console.log('Liquidity pool hash : ', liquidityPoolHash)
+    const swapHash = await polkaClient.swap(1,'D11K.DOJ', inboundAddress, 'dojima15ca4lmfe9u6cc5x0cmqmw2wkvh6l4xdpr908km')
+    console.log('Swap tx hash : ', swapHash)
     process.exit()
 }
 
