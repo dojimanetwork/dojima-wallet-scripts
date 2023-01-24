3f68c75d626fd0a251534eb7f2cf4728a27becef
yerramreddyuday <uday.prasad@dojima.network>
2023-01-24 23:03:47 +0530

    dev: add: Add liquidity pool and swap functionality in Ethereum package

--- src/core/eth_dojima/client.ts
+++ src/core/eth_dojima/client.ts
@@ -5,8 +5,9 @@ import BigNumber from "bignumber.js";
 import {EthTransferParams, EthTxData, GasfeeResult} from "./types";
 import {ChainClientParams} from "@d11k-ts/client";
 import {validatePhrase} from "@d11k-ts/crypto";
-import {defaultEthInfuraRpcUrl, defaultInfuraApiKey} from "./const";
-import {Transaction} from "web3-eth";
+import {defaultEthInfuraRpcUrl, defaultInfuraApiKey, ETH_DECIMAL} from "./const";
+import {InboundAddressResult, SwapAssetList} from "../utils";
+import axios from "axios";
 
 export type EthRpcParams = {
     rpcUrl?: string,
@@ -18,25 +19,25 @@ export default class EthChain {
     protected web3: Web3;
     protected rpcUrl: string;
     protected account: ethers.ethers.Wallet;
-    protected phrase = ''
+    protected phrase = "";
 
     constructor({
                     phrase,
                     network = Network.Mainnet,
                     rpcUrl = defaultEthInfuraRpcUrl,
-                    infuraApiKey = defaultInfuraApiKey
+                    infuraApiKey = defaultInfuraApiKey,
                 }: ChainClientParams & EthRpcParams) {
         if (phrase) {
             if (!validatePhrase(phrase)) {
-                throw new Error('Invalid phrase')
+                throw new Error("Invalid phrase");
             }
-            this.phrase = phrase
+            this.phrase = phrase;
         }
         this.network = network;
-        if ((this.network !== Network.Mainnet) && rpcUrl === defaultEthInfuraRpcUrl) {
-            throw Error(`'rpcUrl' param can't be empty for 'testnet' or 'stagenet'`)
+        if (this.network !== Network.Mainnet && rpcUrl === defaultEthInfuraRpcUrl) {
+            throw Error(`'rpcUrl' param can't be empty for 'testnet' or 'stagenet'`);
         }
-        if(this.network === Network.Testnet || this.network === Network.Stagenet) {
+        if (this.network === Network.Testnet || this.network === Network.Stagenet) {
             this.rpcUrl = rpcUrl;
             this.web3 = new Web3(this.rpcUrl);
         } else {
@@ -51,39 +52,36 @@ export default class EthChain {
     }
 
     async getBalance(address: string): Promise<number> {
-        const gweiBalance = await this.web3.eth.getBalance(address);    // Results balance in gwei, 1 eth = 10^9 gwei(1,000,000,000)
+        const gweiBalance = await this.web3.eth.getBalance(address); // Results balance in gwei, 1 eth = 10^9 gwei(1,000,000,000)
         const ethBalance = this.web3.utils.fromWei(gweiBalance);
         return Number(ethBalance);
     }
 
     calculateDojFee(baseGasFee: number, multiplier: number): number {
-        return (new BigNumber(baseGasFee)
+        return new BigNumber(baseGasFee)
             .times(new BigNumber(multiplier))
-            .toNumber());
+            .toNumber();
     }
 
     async estimateGasFee(amount: number, memo?: string): Promise<number> {
         return await this.web3.eth.estimateGas({
             from: this.getAddress(),
             to: this.getAddress(),
-            value: amount * Math.pow(10, 18),
-            data: memo ? this.web3.utils.toHex(memo) : undefined
+            value: amount * Math.pow(10, ETH_DECIMAL),
+            data: memo ? this.web3.utils.toHex(memo) : undefined,
         });
     }
 
     async getFees(amount: number, memo?: string): Promise<GasfeeResult> {
-        const estimateGas = await this.estimateGasFee(amount, memo ? memo : undefined)
+        const estimateGas = await this.estimateGasFee(
+            amount,
+            memo ? memo : undefined
+        );
         return {
             slow: this.calculateDojFee(estimateGas, 1) / Math.pow(10, 9),
             average: this.calculateDojFee(estimateGas, 1.5) / Math.pow(10, 9),
             fast: this.calculateDojFee(estimateGas, 2) / Math.pow(10, 9),
         };
-        // const baseGasFee = await this.web3.eth.getGasPrice();
-        // const eth_gasFee = {
-        //     slow: this.calculateDojFee(parseFloat(baseGasFee), 1) / Math.pow(10, 18),
-        //     average: this.calculateDojFee(parseFloat(baseGasFee), 1.5) / Math.pow(10, 18),
-        //     fast: this.calculateDojFee(parseFloat(baseGasFee), 2) / Math.pow(10, 18),
-        // };
     }
 
     async transfer(params: EthTransferParams): Promise<string> {
@@ -91,37 +89,93 @@ export default class EthChain {
             {
                 from: this.getAddress(),
                 to: params.recipient,
-                value: params.amount * Math.pow(10, 18),
-                gas: params.fee ? params.fee * Math.pow(10, 9) : await this.estimateGasFee(params.amount, params.memo ? params.memo : undefined),
-                data: params.memo ? this.web3.utils.toHex(params.memo) : undefined
+                value: params.amount * Math.pow(10, ETH_DECIMAL),
+                gas: params.fee
+                    ? params.fee * Math.pow(10, 9)
+                    : await this.estimateGasFee(
+                        params.amount,
+                        params.memo ? params.memo : undefined
+                    ),
+                data: params.memo ? this.web3.utils.toHex(params.memo) : undefined,
             },
             this.account.privateKey
         );
 
         const transactionResult = await this.web3.eth.sendSignedTransaction(
-            transaction.rawTransaction
+            transaction.rawTransaction as string
         );
         return transactionResult.transactionHash;
     }
 
     async getTransactionData(hash: string): Promise<EthTxData> {
-        const data: Transaction = await this.web3.eth.getTransaction(hash);
-        if(data) {
+        const data = await this.web3.eth.getTransaction(hash);
+        if (data) {
             return {
                 transaction_hash: data.hash,
                 from: data.from,
-                to: data.to,
-                amount: Number(data.value) / Math.pow(10, 18),
+                to: data.to as string,
+                amount: Number(data.value) / Math.pow(10, ETH_DECIMAL),
                 gasFee: data.gas / Math.pow(10, 9),
-                block_number: data.blockNumber,
-                block_hash: data.blockHash,
+                block_number: data.blockNumber as number,
+                block_hash: data.blockHash as string,
                 gasPrice: data.gasPrice,
-                nonce: data.nonce
-            }
+                nonce: data.nonce,
+            };
         } else {
-            throw new Error(`Failed to get transaction data (tx-hash: ${hash})`)
+            throw new Error(`Failed to get transaction data (tx-hash: ${hash})`);
         }
-        // const data = await this.web3.eth.getTransactionReceipt(hash);
-        // return data
+    }
+
+    async getInboundObject(): Promise<InboundAddressResult> {
+        const response = await axios.get('https://api-test.h4s.dojima.network/hermeschain/inbound_addresses')
+        if (response.status !== 200) {
+            throw new Error(
+                `Unable to retrieve inbound addresses. Dojima gateway responded with status ${response.status}.`
+            );
+        }
+
+        const data: Array<InboundAddressResult> = response.data;
+        const inboundObj: InboundAddressResult = data.find(res => res.chain === 'ETH')
+        return inboundObj
+    }
+
+    async getEthereumInboundAddress(): Promise<string> {
+        const inboundObj = await this.getInboundObject()
+        return inboundObj.address
+    }
+
+    async getDefaultLiquidityPoolGasFee(): Promise<number> {
+        const inboundObj = await this.getInboundObject()
+
+        const gasFee = Number(inboundObj.gas_rate) / Math.pow(10, ETH_DECIMAL);
+
+        return gasFee;
+    }
+
+    async addLiquidityPool(amount: number, inboundAddress: string, dojAddress?: string): Promise<string> {
+        const memo = dojAddress ?
+            `ADD:ETH.ETH:${dojAddress}`
+            :
+            `ADD:ETH.ETH`
+
+        const txHash = await this.transfer({
+            amount,
+            recipient: inboundAddress,
+            memo
+        })
+
+        return txHash
+    }
+
+    async swap(amount: number, token: SwapAssetList, inboundAddress: string, recipient: string): Promise<string> {
+        const memo = `SWAP:${token}:${recipient}`
+
+        const txHash = await this.transfer({
+            amount,
+            recipient: inboundAddress,
+            memo
+        })
+
+        return txHash
     }
 }
--- src/core/eth_dojima/const.ts
+++ src/core/eth_dojima/const.ts
@@ -1,3 +1,5 @@
 export const defaultEthInfuraRpcUrl = 'https://mainnet.infura.io/v3/'
 
 export const defaultInfuraApiKey = 'f37faaf5ddeb4e589d6f26300ed673a6'
+
+export const ETH_DECIMAL = 18;
